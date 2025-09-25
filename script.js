b392978a4308cf9cee6e23a09b2e88dc8d3d0290
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-24 20:43:25 -0700

    used querySelectAll to select all grids and added eventlistner to the grids i also added a class to colored grid to classify which grids are colored learned how to use for each method to add an eventlistner to all the elements

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ const height = '30px';
 function addGrid () {
     for (let i = 0; i<256 ; i++) {
         let grid = document.createElement('div')
+        grid.classList.add('grid');
         grid.style.border = '1px solid lightgray'
         grid.style.width = width
         grid.style.height = height
         gridContainer.appendChild(grid);
     }
 }
+function changeBgColor (grid) {
+    grid.style.backgroundColor = 'black';
+    grid.classList.add('colored');
+}
 addGrid();
+
+let grids = document.querySelectorAll('.grid');
+grids.forEach((grid) => {grid.addEventListener('mouseenter', (e) => changeBgColor(e.target))});
