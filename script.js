7fe4aef63d9eae0cb0fbea7f0a404a3af1caba21
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-30 15:27:08 -0700

    added 'new grid' button element

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let gridContainer = document.querySelector('#gridContainer')
+let button = document.querySelector('btn');
 const width = '30px';
 const height = '30px';
+//adds the grid
 function addGrid () {
     for (let i = 0; i<256 ; i++) {
         let grid = document.createElement('div')
@@ -11,11 +13,13 @@ function addGrid () {
         gridContainer.appendChild(grid);
     }
 }
+//changes the background color
 function changeBgColor (grid) {
     grid.style.backgroundColor = 'black';
     grid.classList.add('colored');
 }
-addGrid();
-
+addGrid(); // adds the grid to the window
 let grids = document.querySelectorAll('.grid');
-grids.forEach((grid) => {grid.addEventListener('mouseenter', (e) => changeBgColor(e.target))});
+grids.forEach((grid) => {
+    grid.addEventListener('mouseenter', (e) => changeBgColor(e.target))
+}); //set event listener to all grids
