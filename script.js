042117c3cda2d9f99462d1fb4d37f33c3e97d4fd
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-30 15:49:56 -0700

    formatted code with prettier

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-let gridContainer = document.querySelector('#gridContainer')
-let button = document.querySelector('#btn');
-const width = '30px';
-const height = '30px';
+let gridContainer = document.querySelector("#gridContainer");
+let button = document.querySelector("#btn");
+const width = "30px";
+const height = "30px";
 //adds the grid
-function addGrid (num) {
-    for (let i = 0; i<num*num ; i++) {
-        let grid = document.createElement('div')
-        grid.classList.add('grid');
-        grid.style.border = '1px solid lightgray'
-        grid.style.width = 512/num -2 + 'px'
-        grid.style.height = 512/num - 2+ 'px'
-        gridContainer.appendChild(grid);
-    }
-    let grids = document.querySelectorAll('.grid');
-    //set event listener to all grids
-    grids.forEach((grid) => {
-    grid.addEventListener('mouseenter', (e) => changeBgColor(e.target))
-})
+function addGrid(num) {
+  for (let i = 0; i < num * num; i++) {
+    let grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.style.border = "1px solid lightgray";
+    grid.style.width = 512 / num - 2 + "px";
+    grid.style.height = 512 / num - 2 + "px";
+    gridContainer.appendChild(grid);
+  }
+  let grids = document.querySelectorAll(".grid");
+  //set event listener to all grids
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseenter", (e) => changeBgColor(e.target));
+  });
 }
 //changes the background color
-function changeBgColor (grid) {
-    grid.style.backgroundColor = 'black';
-    grid.classList.add('colored');
+function changeBgColor(grid) {
+  grid.style.backgroundColor = "black";
+  grid.classList.add("colored");
 }
 addGrid(16); // adds the grid to the window
 
-
-
 //function to create a new grid
-function newGrid () {
-    num = prompt('How many squares per side?')
-    if (num > 100) {
-        alert('Please have numbers less than 100!')
-        return
-    }
-    let grids = document.querySelectorAll('.grid')
-    grids.forEach((grid) => {
-        gridContainer.removeChild(grid)
-    });
-    addGrid(num);
-    grids = document.querySelectorAll('.grid')
-    grids.forEach((grid) => {
-        grid.addEventListener('mouseenter', (e) => changeBgColor(e.target))
-    })
+function newGrid() {
+  num = prompt("How many squares per side?");
+  if (num > 100) {
+    alert("Please have numbers less than 100!");
+    return;
+  }
+  let grids = document.querySelectorAll(".grid");
+  grids.forEach((grid) => {
+    gridContainer.removeChild(grid);
+  });
+  addGrid(num);
+  grids = document.querySelectorAll(".grid");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseenter", (e) => changeBgColor(e.target));
+  });
 }
 //set event listner to new grid button
-button.addEventListener('click', () => newGrid())
+button.addEventListener("click", () => newGrid());
